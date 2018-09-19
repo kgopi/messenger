db19d2a38a275169ba836346168573ad1e5d0e1e
gs-kgopi <kgopi@gainsight.com>
2018-09-19 10:50:37 +0530

    sql scripts patch #2

--- config/db.js
+++ config/db.js
@@ -1,25 +1,38 @@
 var sql = require('sql');
 
+
 /**
  * SQL definition for public.event
  */
 exports.event = sql.define({
-  name: 'event',
-  schema: 'public',
-  columns: [
-    { name: 'id', property: 'id' },
-    { name: 'area', property: 'area' },
-    { name: 'name', property: 'name' },
-    { name: 'actor_name', property: 'actorName' },
-    { name: 'actor_id', property: 'actorId' },
-    { name: 'priority', property: 'priority' },
-    { name: 'title', property: 'title' },
-    { name: 'body', property: 'body' },
-    { name: 'email_body', property: 'email_body' },
-    { name: 'icon', property: 'icon' },
-    { name: 'time_to_live', property: 'timeToLive' },
-    { name: 'data', property: 'data' }
-  ]
+        name: 'event',
+        columns: [
+                { name: 'id', property: 'id' },
+                { name: 'area', property: 'area' },
+                { name: 'name', property: 'name' },
+                { name: 'actor_id', property: 'actorId' },
+                { name: 'actor_name', property: 'actorName' },
+                { name: 'priority', property: 'priority' },
+                { name: 'time_to_live', property: 'timeToLive' },
+                { name: 'title', property: 'title' },
+                { name: 'body', property: 'body' },
+                { name: 'data', property: 'data' },
+                { name: 'date_created', property: 'dateCreated' }
+        ]
+});
+
+
+/**
+ * SQL definition for public.preferences
+ */
+exports.preferences = sql.define({
+        name: 'preferences',
+        columns: [
+                { name: 'id', property: 'id' },
+                { name: 'user_id', property: 'userId' },
+                { name: 'is_admin', property: 'isAdmin' },
+                { name: 'data', property: 'data' }
+        ]
 });
 
 
@@ -27,30 +40,14 @@ exports.event = sql.define({
  * SQL definition for public.user_event_mapping
  */
 exports.userEventMapping = sql.define({
-  name: 'user_event_mapping',
-  schema: 'public',
-  columns: [
-    { name: 'event_id', property: 'eventId' },
-    { name: 'user_id', property: 'userId' },
-    { name: 'is_admin', property: 'isAdmin' },
-    { name: 'is_read', property: 'isRead' }
-    { name: 'email', property: 'email' },
-    { name: 'read_via', property: 'readVia' },
-    { name: 'read_on', property: 'readOn' }
-  ]
+        name: 'user_event_mapping',
+        columns: [
+                { name: 'event_id', property: 'eventId' },
+                { name: 'user_id', property: 'userId' },
+                { name: 'is_admin', property: 'isAdmin' },
+                { name: 'is_read', property: 'isRead' },
+                { name: 'email', property: 'email' },
+                { name: 'read_via', property: 'readVia' },
+                { name: 'read_on', property: 'readOn' }
+        ]
 });
-
-/**
- * SQL definition for public.preferences
- */
-exports.preferences = sql.define({
-    name: 'preferences',
-    schema: 'public',
-    columns: [
-      { name: 'id', property: 'id' },
-      { name: 'user_id', property: 'userId' },
-      { name: 'config', property: 'config' }
-    ]
-  });
-
-
--- dbscript/messenger.sql
+++ dbscript/messenger.sql
@@ -10,17 +10,16 @@ CREATE SEQUENCE "event_id_seq" INCREMENT 1 START 2 MAXVALUE 9223372036854775807 
 DROP TABLE IF EXISTS "event" CASCADE;
 CREATE TABLE "event" (
 	"id" int8 NOT NULL DEFAULT nextval('event_id_seq'::regclass) PRIMARY KEY,
-	"area" varchar(100) NOT NULL,
-    "name" varchar(100) NOT NULL,
-    "actor_id" varchar(255) NOT NULL,
+	"area" varchar(50) NOT NULL,
+    "name" varchar(50) NOT NULL,
+    "actor_id" varchar(32) NOT NULL,
     "actor_name" varchar(255) NOT NULL,
     "priority" varchar(10) DEFAULT 'NORMAL',
 	"time_to_live" int2 DEFAULT 365,
     "title" varchar(4000) COLLATE "default",
     "body" text COLLATE "default",
     "data" text COLLATE "default",
-	"date_created" timestamp(6) NOT NULL DEFAULT now(),
-	"date_updated" timestamp(6) NOT NULL DEFAULT now()
+	"date_created" timestamp(6) NOT NULL DEFAULT now()
 )
 WITH (OIDS=FALSE);
 
@@ -30,12 +29,24 @@ WITH (OIDS=FALSE);
 DROP TABLE IF EXISTS "user_event_mapping" CASCADE;
 CREATE TABLE "user_event_mapping" (
 	"event_id" int8 NOT NULL REFERENCES "event" (id),
-    "user_id" varchar(255) NOT NULL,
+    "user_id" varchar(32) NOT NULL,
     "is_admin" bool DEFAULT false,
     "is_read" bool DEFAULT false,
 	"email" varchar(250) NOT NULL,
     "read_via" varchar(50) NOT NULL,
     "read_on" timestamp(6) NOT NULL,
     PRIMARY KEY("event_id", "user_id")
+)
+WITH (OIDS=FALSE);
+
+-- ----------------------------
+--  Table structure for preferences
+-- ----------------------------
+DROP TABLE IF EXISTS "preferences" CASCADE;
+CREATE TABLE "preferences" (
+	"id" SERIAL PRIMARY KEY,
+    "user_id" varchar(32) NOT NULL UNIQUE,
+    "is_admin" bool DEFAULT false,
+    "data" text COLLATE "default"
 )
 WITH (OIDS=FALSE);
